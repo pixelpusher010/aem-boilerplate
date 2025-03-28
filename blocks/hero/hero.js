781aa764e1e2eb92cea6121b2c50e9fcd948038f
Marko Kocic <kocic@adobe.com>
2025-03-28 13:43:05 +0100

    hero-test

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,12 @@
 const isActiveExperiment = () => document.body.classList.contains('experiment-hero-test');
 const isChallenger = () => document.body.classList.contains('variant-challenger-1');
 
+const HTMLContent = `
+    <div class="rounded-element">Rounded Element</div>
+    `;
 export default function decorate(block) {
     if(isActiveExperiment() && isChallenger()) {
-      block.innerHTML = '<div class="rounded-element">Rounded Element</div>';
+        block.style.setProperty('style', 'display: grid; place-items: center;');
+        block.innerHTML = '<div class="rounded-element">Rounded Element</div>';
     }
 }
